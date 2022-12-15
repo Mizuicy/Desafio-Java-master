dcc6578725508e362bdb7caab8c8f80a3dd7b6b2
CTT <gustavosilva90@gmail.com>
2022-12-15 16:10:36 -0300

    começado de novo

--- src/main/java/br/com/totvs/juridico/LivrariaApplication.java
+++ src/main/java/br/com/totvs/juridico/LivrariaApplication.java
@@ -3,55 +3,69 @@ package br.com.totvs.juridico;
 import dto.LivroDTO;
 import java.util.ArrayList;
 
+import javax.xml.validation.Validator;
+
 public class LivrariaApplication {
-    ArrayList<LivroDTO> listLivros= new ArrayList<>();
-    public static void main(String[] args){
+    ArrayList<LivroDTO> listLivros = new ArrayList<>();
+
+    public static void main(String[] args) {
         new LivrariaApplication();
     }
 
-    public LivrariaApplication(){
+    public LivrariaApplication() {
         this.initLivros();
         this.criarUmNovoLivro("Arquitetura Limpa", "Tecnologia");
         this.criarUmNovoLivro("Harry Potter e a Camara secreta", "Fantasia");
         this.criarUmNovoLivro("WildCards", "Esportes");
         this.criarUmNovoLivro("O Trono do Sol", "Fantasia");
-        System.out.println(this.listarLivrosFantasia());
+
     }
 
-    public void initLivros(){
-        this.listLivros.add(new LivroDTO( 0,  "Código Limpo", "Tecnologia"));
-        this.listLivros.add(new LivroDTO( 1,  "Senhor do Anéis",  "Fantasia" ));
-        this.listLivros.add(new LivroDTO( 2,  "WildCards", "Fantasia" ));
-        this.listLivros.add(new LivroDTO( 3,  "Harry Potter e o Prisioneiro de Azkaban", "Fantasia" ));
-        this.listLivros.add(new LivroDTO( 4,  "Javascript de alto desempenho", "Tecnologia" ));
-        this.listLivros.add(new LivroDTO( 5,  "O poder da ação", "Administração" ));
+    public void initLivros() {
+        this.listLivros.add(new LivroDTO(0, "Código Limpo", "Tecnologia"));
+        this.listLivros.add(new LivroDTO(1, "Senhor do Anéis", "Fantasia"));
+        this.listLivros.add(new LivroDTO(2, "WildCards", "Fantasia"));
+        this.listLivros.add(new LivroDTO(3, "Harry Potter e o Prisioneiro de Azkaban", "Fantasia"));
+        this.listLivros.add(new LivroDTO(4, "Javascript de alto desempenho", "Tecnologia"));
+        this.listLivros.add(new LivroDTO(5, "O poder da ação", "Administração"));
     }
 
+    /**
+     * @param nome
+     * @param genero
+     */
     public void criarUmNovoLivro(String nome, String genero){
-        if (buscarLivro(nome, genero) == 0){
+        
+            for (int index = 0; index < this.listLivros.size(); index++) {
+                LivroDTO livro = this.listLivros.get(index);
+            if (buscarLivro(nome, genero) == 0) {
             LivroDTO novoLivro = new LivroDTO(this.listLivros.size(), nome, genero);
             this.listLivros.add(novoLivro);
         } else {
             /**
-             * Monte a mensagem de erro avisando que já existe um Livro cadastrado sobre o nome e genero passados
+             * Monte a mensagem de erro avisando que já existe um Livro cadastrado sobre o
+             * nome e genero passados
              */
-            System.out.println("O livro "+ nome + " do genero " + genero + " ja foi cadastrado na lista ");
+
         }
     }
+}
 
-    public int buscarLivro(String nomeLivro, String genero){
+    public int buscarLivro(String nomeLivro, String genero) {
+
         int indiceLivro = -1;
-
-        for (int index = 0 ; index < this.listLivros.size(); index++){
+        for (int index = 0; index < this.listLivros.size(); index++) {
             LivroDTO livro = this.listLivros.get(index);
             /**
-             * Implemente a validação onde retorne o Indice do Livro caso encontre um com mesmo nome e genero
-             */
+                                        * Implemente a validação onde retorne o Indice do Livro caso encontre um com
+                                        * mesmo nome e genero
+                                        */
         }
         return indiceLivro;
     }
 
-    public String listarLivrosFantasia(){
+    public String listarLivrosFantasia() {
+        int livrosDeFantasia;
         return null;
         /**
          * Retorne uma String contendo todos os Livros que são de fantasia.
--- src/main/java/dto/LivroDTO.java
+++ src/main/java/dto/LivroDTO.java
@@ -2,8 +2,9 @@ package dto;
 
 public class LivroDTO {
     int id;
-    String nome;
-    String genero;
+    public String nome;
+    public String genero;
+
     public LivroDTO(int id, String nome, String genero){
         this.id = id;
         this.nome = nome;
